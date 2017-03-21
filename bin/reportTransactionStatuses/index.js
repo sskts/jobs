421eef9f7cc77d1181ea491d9c5463f88f8d6bcc
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-03-21 23:40:07 +0900

    SSKTS-173 取引ステータスレポート送信ジョブ調整

--- bin/reportTransactionStatuses/index.js
+++ bin/reportTransactionStatuses/index.js
@@ -23,16 +23,18 @@ function main() {
     return __awaiter(this, void 0, void 0, function* () {
         debug('connecting mongodb...');
         mongoose.connect(process.env.MONGOLAB_URI, mongooseConnectionOptions_1.default);
+        const queueAdapter = sskts.adapter.queue(mongoose.connection);
         const transactionAdapter = sskts.adapter.transaction(mongoose.connection);
         debug('creating a report...');
-        const report = yield sskts.service.report.transactionStatuses()(transactionAdapter);
+        const report = yield sskts.service.report.transactionStatuses()(queueAdapter, transactionAdapter);
         yield sskts.service.notification.report2developers('現在の取引集計', `
 date： ${moment().toISOString()}\n
 \n
-取引在庫数: ${report.nubmerOfTransactionsReady}\n
-進行中取引数: ${report.nubmerOfTransactionsUnderway}\n
-キュー未エクスポートの成立済み取引数: ${report.nubmerOfTransactionsClosedWithQueuesUnexported}\n
-キュー未エクスポートの期限切れ取引数: ${report.nubmerOfTransactionsExpiredWithQueuesUnexported}\n
+取引在庫数: ${report.numberOfTransactionsReady}\n
+進行中取引数: ${report.numberOfTransactionsUnderway}\n
+キュー未エクスポートの成立済み取引数: ${report.numberOfTransactionsClosedWithQueuesUnexported}\n
+キュー未エクスポートの期限切れ取引数: ${report.numberOfTransactionsExpiredWithQueuesUnexported}\n
+未実行キュー数: ${report.numberOfQueuesUnexecuted}\n
 `)();
         mongoose.disconnect();
     });
--- bin/reportTransactionStatuses/index.ts
+++ bin/reportTransactionStatuses/index.ts
@@ -16,18 +16,20 @@ async function main() {
     debug('connecting mongodb...');
     mongoose.connect(process.env.MONGOLAB_URI, mongooseConnectionOptions);
 
+    const queueAdapter = sskts.adapter.queue(mongoose.connection);
     const transactionAdapter = sskts.adapter.transaction(mongoose.connection);
     debug('creating a report...');
-    const report = await sskts.service.report.transactionStatuses()(transactionAdapter);
+    const report = await sskts.service.report.transactionStatuses()(queueAdapter, transactionAdapter);
     await sskts.service.notification.report2developers(
         '現在の取引集計',
         `
 date： ${moment().toISOString()}\n
 \n
-取引在庫数: ${report.nubmerOfTransactionsReady}\n
-進行中取引数: ${report.nubmerOfTransactionsUnderway}\n
-キュー未エクスポートの成立済み取引数: ${report.nubmerOfTransactionsClosedWithQueuesUnexported}\n
-キュー未エクスポートの期限切れ取引数: ${report.nubmerOfTransactionsExpiredWithQueuesUnexported}\n
+取引在庫数: ${report.numberOfTransactionsReady}\n
+進行中取引数: ${report.numberOfTransactionsUnderway}\n
+キュー未エクスポートの成立済み取引数: ${report.numberOfTransactionsClosedWithQueuesUnexported}\n
+キュー未エクスポートの期限切れ取引数: ${report.numberOfTransactionsExpiredWithQueuesUnexported}\n
+未実行キュー数: ${report.numberOfQueuesUnexecuted}\n
 `
     )();
 
