a93846f3349d7a324581109cde307ecf18619eb4
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-05-20 15:46:04 +0900

    fix tslint@^5.2.0

---
+++ test/index-test.js
@@ -0,0 +1,16 @@
+"use strict";
+/**
+ * テストサンプル
+ *
+ * @ignore
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const assert = require("assert");
+// import * as mongoose from 'mongoose';
+// before(async () => {
+// });
+describe('sample test', () => {
+    it('ok', () => {
+        assert(true);
+    });
+});
---
+++ test/index-test.ts
@@ -0,0 +1,17 @@
+/**
+ * テストサンプル
+ *
+ * @ignore
+ */
+
+import * as assert from 'assert';
+// import * as mongoose from 'mongoose';
+
+// before(async () => {
+// });
+
+describe('sample test', () => {
+    it('ok', () => {
+        assert(true);
+    });
+});
