a39643f25b88f1e42f6c332a329e3726d126e0f0
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-05-27 14:32:57 +0000

    # SSKTS-927 ### Added - 会員プログラム登録ジョブを追加。

--- dst/jobs/continuous/registerProgramMembership/run.js
+++ dst/jobs/continuous/registerProgramMembership/run.js
@@ -12,6 +12,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * 会員プログラム登録タスク
  */
 const sskts = require("@motionpicture/sskts-domain");
+const AWS = require("aws-sdk");
 const createDebug = require("debug");
 const mongooseConnectionOptions_1 = require("../../../mongooseConnectionOptions");
 const debug = createDebug('sskts-jobs:*');
@@ -20,6 +21,14 @@ let count = 0;
 const MAX_NUBMER_OF_PARALLEL_TASKS = 10;
 const INTERVAL_MILLISECONDS = 200;
 const taskRepo = new sskts.repository.Task(sskts.mongoose.connection);
+const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({
+    apiVersion: 'latest',
+    region: 'ap-northeast-1',
+    credentials: new AWS.Credentials({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    })
+});
 setInterval(() => __awaiter(this, void 0, void 0, function* () {
     if (count > MAX_NUBMER_OF_PARALLEL_TASKS) {
         return;
@@ -28,7 +37,8 @@ setInterval(() => __awaiter(this, void 0, void 0, function* () {
     try {
         yield sskts.service.task.executeByName(sskts.factory.taskName.RegisterProgramMembership)({
             taskRepo: taskRepo,
-            connection: sskts.mongoose.connection
+            connection: sskts.mongoose.connection,
+            cognitoIdentityServiceProvider: cognitoIdentityServiceProvider
         });
     }
     catch (error) {
--- src/jobs/continuous/registerProgramMembership/run.ts
+++ src/jobs/continuous/registerProgramMembership/run.ts
@@ -2,6 +2,7 @@
  * 会員プログラム登録タスク
  */
 import * as sskts from '@motionpicture/sskts-domain';
+import * as AWS from 'aws-sdk';
 import * as createDebug from 'debug';
 
 import mongooseConnectionOptions from '../../../mongooseConnectionOptions';
@@ -15,6 +16,14 @@ let count = 0;
 const MAX_NUBMER_OF_PARALLEL_TASKS = 10;
 const INTERVAL_MILLISECONDS = 200;
 const taskRepo = new sskts.repository.Task(sskts.mongoose.connection);
+const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({
+    apiVersion: 'latest',
+    region: 'ap-northeast-1',
+    credentials: new AWS.Credentials({
+        accessKeyId: <string>process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: <string>process.env.AWS_SECRET_ACCESS_KEY
+    })
+});
 
 setInterval(
     async () => {
@@ -29,7 +38,8 @@ setInterval(
                 sskts.factory.taskName.RegisterProgramMembership
             )({
                 taskRepo: taskRepo,
-                connection: sskts.mongoose.connection
+                connection: sskts.mongoose.connection,
+                cognitoIdentityServiceProvider: cognitoIdentityServiceProvider
             });
         } catch (error) {
             console.error(error);
