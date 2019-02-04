cc5dd4ff951d7a71bd74fa0b0756c40cdc7bd9f6
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-02-04 00:35:17 +0000

    # SSKTS-1141 ### Changed - install @motionpicture/sskts-domain@28.0.0

---
+++ src/jobs/continuous/confirmReservation/run.ts
@@ -0,0 +1,45 @@
+/**
+ * 予約確定
+ */
+import * as sskts from '@motionpicture/sskts-domain';
+import * as createDebug from 'debug';
+
+import mongooseConnectionOptions from '../../../mongooseConnectionOptions';
+
+const debug = createDebug('sskts-jobs:*');
+
+sskts.mongoose.connect(<string>process.env.MONGOLAB_URI, mongooseConnectionOptions)
+    .then(debug)
+    // tslint:disable-next-line:no-console
+    .catch(console.error);
+
+let count = 0;
+
+const MAX_NUBMER_OF_PARALLEL_TASKS = 10;
+const INTERVAL_MILLISECONDS = 200;
+const taskRepo = new sskts.repository.Task(sskts.mongoose.connection);
+
+setInterval(
+    async () => {
+        if (count > MAX_NUBMER_OF_PARALLEL_TASKS) {
+            return;
+        }
+
+        count += 1;
+
+        try {
+            await sskts.service.task.executeByName(
+                sskts.factory.taskName.ConfirmReservation
+            )({
+                taskRepo: taskRepo,
+                connection: sskts.mongoose.connection
+            });
+        } catch (error) {
+            // tslint:disable-next-line:no-console
+            console.error(error);
+        }
+
+        count -= 1;
+    },
+    INTERVAL_MILLISECONDS
+);
